31bc03eb3b51906e1c988be7ef56fb9a98518405
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-10-17 22:42:10 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan1-Biodata/src/pboif2/pkg10119048/latihan1/biodata/PBOIF210119048Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * Nama      : Mochammad Faishal
+ * Kelas     : IF2
+ * NIM       : 10119048
+ * Deskripsi : Menampilkan Biodata
+ */
+public class PBOIF210119048Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA PENDAKI" + "\n" + "-----------------");
+        System.out.println("NAMA\t\t : Mochammad Faishal ");
+        System.out.println("ALAMAT\t\t : Cililin Bandung Barat ");
+        System.out.println("TANGGAL LAHIR\t : 24 November 2000");
+        System.out.println("TEMPAT LAHIR\t : Bandung");
+        System.out.println("------------------------");
+        System.out.println("HOBI\t\t : Bercocok Tanam");
+        System.out.println("Makanan Kesukaan\t : Gehu");
+        System.out.println("Minuman Kesukaan\t : Teh");
+    }
+    
+}
